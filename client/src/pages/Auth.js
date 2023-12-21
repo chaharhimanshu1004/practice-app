cc7a9e72dad7731aed8895ab21f4af6f68e4cd0c
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-21 11:55:03 +0530

    login function added

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -7,10 +7,61 @@ export const Auth=()=>{
     return(
         <div className="auth">
             <Login></Login>
-            {/* <Register></Register> */}
+            <Register></Register>
         </div>
     )
 };
+
+const Register = ()=>{
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event)=>{
+        event.preventDefault();
+        try{
+            const result = axios.post("http://localhost:3003/auth/register",{
+                username,
+                password,
+            })
+            alert('Registration Completed !, Now Login');
+            setUsername("");
+            setPassword("");
+            
+
+        }catch(err){
+            console.error(err);
+        }
+        
+
+    }
+
+    return(
+    <div className="auth-container">
+      <form onSubmit={handleSubmit} >
+        <h2>Register</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+    )
+}
 
 const Login= ()=>{
     const [_, setCookies] = useCookies(["access_token"]);
@@ -25,10 +76,10 @@ const Login= ()=>{
                 username,
                 password,
             })
-            setCookies("access_token", result.data.token);
-            window.localStorage.setItem("userID", result.data.userID);
+            console.log(result)
+            // setCookies("access_token", result.data.token);
+            // window.localStorage.setItem("userID", result.data.userID);
             navigate("/");
-            
 
         }catch(err){
             console.error(err);
@@ -47,7 +98,6 @@ const Login= ()=>{
             id="username"
             value={username}
             onChange={(event)=>setUsername(event.target.value)}
-            
           />
         </div>
         <div className="form-group">
